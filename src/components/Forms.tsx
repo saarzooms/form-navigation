0898ee4d6fc86b4130b8bc1ffd3feb04afe42219
arzoo.shaikh <arzoo.rkcet@gmail.com>
2023-04-11 12:56:52 +0530

    form validation

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -6,11 +6,55 @@ const Forms = () => {
         email:"",
         phone:""
     })
+    const [errorData, setErrorData] = useState({
+        name:"",
+        email:"",
+        phone:""
+    })
+    const validateForm =()=>{
+        var isValid = true;
+        const newError = {...errorData}
+        
+        //name validation
+        if(!formData.name){
+            newError.name='Please enter your name'
+            isValid = false
+        }else{
+            newError.name = ''
+        }
+
+        //email
+        if(!formData.email){
+            newError.email='Please enter your email'
+            isValid = false
+        }else if(!/\S+@\S+\.\S+/.test(formData.email)){
+            newError.email = 'Please eneter valid email'
+            isValid = false
+        }else{
+            newError.email = ''
+        }
+        //phone 
+        if(!formData.phone){
+            newError.phone='Please enter your phone number'
+            isValid = false
+        }else if((formData.phone.length<10)){
+            newError.phone = 'Please eneter valid phone number'
+            isValid = false
+        }else{
+            newError.phone = ''
+        }
+
+        setErrorData(newError)
+        return isValid
+    }
     const handleSubmit= (event:FormEvent)=>{
         event.preventDefault();
         console.log(formData);
-        
+        if(validateForm()){
+
+        }
     }
+    
     const handleInput = (event:ChangeEvent<HTMLInputElement>) =>{
         const {name,value}= event.target;
         setFormData((prevData)=>({
@@ -24,13 +68,16 @@ const Forms = () => {
             <div>
                 <label htmlFor="name" >Name</label>
                 <input name="name" value={formData.name} id="name" type="text" onChange={handleInput}/>
+                {errorData.name && <div className="error">{errorData.name}</div>}
             </div> 
             <div>
                 <label htmlFor="email" >Email</label>
-                <input name="email" value={formData.email} id="email" type="email" onChange={handleInput}/>
+                <input name="email" value={formData.email} id="email" type="text" onChange={handleInput}  />
+                {errorData.email && <div className="error">{errorData.email}</div>}
             </div> <div>
-                <label htmlFor="phone" >Name</label>
-                <input name="phone" value={formData.phone} id="phone" type="number" onChange={handleInput}/>
+                <label htmlFor="phone" >Phone</label>
+                <input name="phone" value={formData.phone} id="phone" type="number" onChange={handleInput}  />
+                {errorData.phone && <div className="error">{errorData.phone}</div>}
             </div>
             <button type="submit">Submit</button>
         </form>
